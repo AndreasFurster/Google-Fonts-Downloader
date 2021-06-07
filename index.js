b6677bcba882edf78c97e38ea0d50bd682ca260a
Andreas Furster <andreas.furster@hotmail.com>
2021-06-07 10:53:04 +0200

    Throttled

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ require('dotenv').config()
 const Axios = require('axios')
 const Path = require('path')
 const Fs = require('fs')
+const pThrottle = require('p-throttle');
 
 const apiKey = process.env.GOOGLE_API_KEY;
 const downloadAmount = process.env.DOWNLOAD_AMOUND;
+
+// Every 30s download a max of 100 files
+const throttle = pThrottle({
+	limit: 100,
+	interval: 30000
+});
+
+const now = Date.now();
+
+const throttled = throttle(index => {
+	const secDiff = ((Date.now() - now) / 1000).toFixed();
+	return Promise.resolve(`${index}: ${secDiff}s`);
+});
+
 
 async function download() {
   if (!apiKey) {
@@ -17,9 +32,11 @@ async function download() {
     await makeFontsDir()
     let response = await Axios.get(`https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=${apiKey}`)
     let items = response.data.items;
-    let promises = []
     let fontsToDownload = Math.min(items.length, downloadAmount || items.length)
     
+    let totalFontFileCount = 0;
+    let currentFontFileCount = 0;
+
     // For each font (or top n fonts)
     for (let i = 0; i < fontsToDownload; i++) {
       const font = items[i];
@@ -27,14 +44,17 @@ async function download() {
 
       // For each file
       for (const [variant, url] of Object.entries(font.files)) {
-        var promise = downloadFont(url, family, variant);
-        promises.push(promise)
+        totalFontFileCount += 1;
+
+        const promise = () => downloadFont(url, family, variant)
+          .then(() => console.log(`Finished ${currentFontFileCount++}/${totalFontFileCount}`))
+          .catch(e => console.log(`Could not download ${family}/${variant}`));
+
+        throttled(totalFontFileCount).then(promise)
       }
     }
 
     console.log(`Downloading ${promises.length} font files from ${fontsToDownload} families... Estimated download size: ${promises.length * 0.3} MB.`);
-    await Promise.all(promises)
-    console.log(`Done!`);
   } catch (error) {
     console.log(error);
   }
